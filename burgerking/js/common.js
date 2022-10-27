8eef90222180b29127c4f928037aa0451e90cfb9
eunmiyo <good3103@gmail.com>
2022-10-27 14:26:28 +0900

    수업

--- burgerking/js/common.js
+++ burgerking/js/common.js
@@ -1,26 +1,27 @@
 /* 1. pc버전 서브메뉴 관련 */
 
-const menu = document.querySelector('.menu__main');
-const subMenu = document.querySelectorAll('.list__drop');
-const panel = document.querySelector('.header__panel');
-const header = document.querySelector('.header');
+const menu = document.querySelector('.menu__main'); /* 전체메뉴 */
+const subMenus = document.querySelectorAll('.list__drop'); /* 하위메뉴 */
+const panel = document.querySelector('.header__panel'); /* 하위 메뉴 나올 때 나오는 판넬 */
+const header = document.querySelector('.header'); /* 전체 헤더 */
 
 // 메뉴에 마우스 오버시 하위메뉴 보여줌
-menu.addEventListener('mouseover', () => {
+menu.addEventListener('mouseover', function() {
     panel.style.display = 'block';
-    subMenu.forEach(subMenu => { subMenu.style.display = "block" });
+    subMenus.forEach(function(subMenu) { 
+        subMenu.style.display = "block" });
 });
 
 // 메뉴 이외의 곳에 마우스 떼면 하위 메뉴 숨김
 header.addEventListener('mouseout', () => {
 	panel.style.display = 'none';
-    subMenu.forEach(subMenu => { subMenu.style.display = "none" });
+    subMenus.forEach(subMenu => { subMenu.style.display = "none" });
 });
 
 // 하위메뉴에서 마우스 떼면 하위메뉴 숨김
 panel.addEventListener('mouseout', () => {
 	panel.style.display = 'none';
-    subMenu.forEach(subMenu => { subMenu.style.display = "none" });
+    subMenus.forEach(subMenu => { subMenu.style.display = "none" });
 });
 
 
@@ -79,7 +80,7 @@ window.addEventListener("optimizedResize", function() {
 //getComputedStyle 사용 이유: https://stackoverflow.com/questions/39679753/javascript-document-getelementbyidel-style-display-returns-empty-string-but
 
 const showHide = e => {
-    const MListDrop = e.children[2];
+    const MListDrop = e.children[2]; /* 내가 선택한 메뉴의 하위메뉴 */
     const displayAttr = window.getComputedStyle(MListDrop).display;
 
     if (displayAttr == 'none') {
