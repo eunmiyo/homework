e2d04390c3f1352ca3e0a3489af373bb0cdc1293
eunmiyo <good3103@gmail.com>
2022-11-01 11:28:51 +0900

    수업

--- project/maket/js/main.js
+++ project/maket/js/main.js
@@ -15,4 +15,13 @@ var swiper = new Swiper(".mySwiper", {
     speed: 800
   });
 
-  
+var slides = document.querySelector('.slides'),
+  slide = document.querySelectorAll('.slides li'),
+  currentIdx = 0,
+  slideCount = slide.length,
+  prevBtn = document.querySelector('.prev'),
+  slideWidth = 200,
+  slideMargin = 30,
+  nextBtn = document.querySelector('.next');
+
+  slides.style.width = (slideWidth + slideMargin)*slideCount - slideMargin;
