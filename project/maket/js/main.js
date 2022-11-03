4db88bf322960e1f6bb747f87ed3182e02482421
eunmiyo <good3103@gmail.com>
2022-11-03 10:05:18 +0900

    up

--- project/maket/js/main.js
+++ project/maket/js/main.js
@@ -48,16 +48,24 @@ function dispToggle(objId) {
   }
 }
 
-// let header = document.querySelector("#header_wrap");
-// let headerHeight = header.offsetHeight;
-// let fixed = document.querySelector('#fix_menu')
-
-// window.onscroll = function() {
-//   let windowTop = window.scrollY;
-//   if (windowTop >= headerHeight) {
-//     fixed.style.display = "block";
-//   } 
-// }
+// fix 상단바 보이고 사라지기
+function navigo() {
+  const header = document.querySelector(".header_wrap"); //헤더부분획득
+  const headerheight = header.clientHeight; //헤더높이
+  document.addEventListener("scroll", onScroll, { passive: true }); //스크롤 이벤트
+  function onScroll() {
+    const scrollposition = pageYOffset; //스크롤 위치
+    const nav = document.querySelector(".fix_menu"); //네비게이션
+    if (headerheight <= scrollposition) {
+      //만약 헤더높이<=스크롤위치라면
+      nav.style.display = "block"; //fix 메뉴 보이기
+    } else {
+      //그 외의 경우
+      nav.style.display = "none"; //fix 메뉴 가리기
+    }
+  }
+}
+navigo();
 
 function dispShow(objId) {
   var obj = document.getElementById(objId);
